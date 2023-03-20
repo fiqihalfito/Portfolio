3d4fecafb155ba7927d522c33fc0121080a897d4
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-03-21 00:46:13 +0700

    test loading.js nextjs 13

---
+++ src/app/portfolio/loading.js
@@ -0,0 +1,6 @@
+
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <p className="text-white text-2xl font-bold font-satisfy">Loading ...</p>
+}
