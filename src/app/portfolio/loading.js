a105776e18f95511a6f80295f8acf05b07496a22
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-03-21 12:46:56 +0700

    loading ui

--- src/app/portfolio/loading.js
+++ src/app/portfolio/loading.js
@@ -2,5 +2,21 @@
 
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
-    return <p className="text-white text-2xl font-bold font-satisfy">Loading ...</p>
+    return (
+        <div className="grid grid-cols-3 gap-12 mt-8">
+            {Array.from({ length: 9 }).map((el) => (
+                <div className="rounded-lg overflow-hidden">
+                    <div className="h-52 bg-neutral-800">
+                        {/* photo placeholder */}
+                    </div>
+                    <div className="bg-neutral-900 p-8">
+                        {/* text placeholder */}
+                        <div className="h-4 w-full bg-neutral-500 mb-4"></div>
+                        <div className="h-4 w-1/2 bg-neutral-500"></div>
+                    </div>
+                </div>
+            ))}
+        </div>
+
+    )
 }
