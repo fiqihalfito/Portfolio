a0f2b0bddca105f0df2fbf9406e5e83d4dd78c89
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-03-22 11:15:13 +0700

    minor fixing

--- src/app/_root_components/NavBar.jsx
+++ src/app/_root_components/NavBar.jsx
@@ -9,12 +9,12 @@ const NavBar = ({ segment }) => {
             <nav className="grid grid-flow-col font-bold">
                 <Link
                     href={'/'}
-                    className={`${segment === 'home' && 'border-b-2'} py-8 flex justify-center w-32`}>
+                    className={`${segment === 'home' ? 'border-b-2' : ''} py-8 flex justify-center w-32`}>
                     Home
                 </Link>
                 <Link
                     href={'/portfolio'}
-                    className={`${segment === 'portfolio' && 'border-b-2'} py-8 flex justify-center w-32`}>
+                    className={`${segment === 'portfolio' ? 'border-b-2' : ''} py-8 flex justify-center w-32`}>
                     Portfolio
                 </Link>
             </nav>
