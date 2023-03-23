be1e64211b599a56618774a7e90f1e5d6e7fa47c
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-03-23 15:18:46 +0700

    minor update

--- src/app/_root_components/SkillAccordion.jsx
+++ src/app/_root_components/SkillAccordion.jsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 
 const SkillAccordion = () => {
 
-    const [tabShow, setTabShow] = useState(1)
+    const [tabShow, setTabShow] = useState(0)
 
     const handleClick = (i) => {
         if (i === tabShow) {
@@ -21,7 +21,7 @@ const SkillAccordion = () => {
             {skills.map((skill, i) => (
                 <div className="group border-2 border-b-0 last:border-b-2 last:rounded-b-lg first:rounded-t-lg py-4 px-6">
                     <div className=" flex justify-between items-center ">
-                        <p className="font-bold">{skill.name}</p>
+                        <p className="font-bold text-xl">{skill.name}</p>
 
                         <button
                             className="px-4 py-1 border-2 rounded-full text-xs invisible group-hover:visible hover:border-amber-400 hover:text-amber-500 hover:bg-amber-50"
@@ -32,7 +32,7 @@ const SkillAccordion = () => {
 
 
                     <div className={`${tabShow === i ? 'max-h-96 duration-700' : 'max-h-0 duration-300'} overflow-hidden text-justify transition-all ease-in-out `}>
-                        <p className="text-xs pt-2">{skill.desc}</p>
+                        <p className="text-sm pt-2">{skill.desc}</p>
                     </div>
 
 
