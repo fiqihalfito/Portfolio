828f4023c0586859bf0c8641d1134a5ee7dc6875
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-03-23 15:14:59 +0700

    add accordion in skill section

---
+++ src/app/_root_components/SkillAccordion.jsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { skills } from "@/lib/skills"
+import { useState } from "react"
+
+const SkillAccordion = () => {
+
+    const [tabShow, setTabShow] = useState(1)
+
+    const handleClick = (i) => {
+        if (i === tabShow) {
+            setTabShow(-1)
+            return
+        }
+
+        setTabShow(i)
+    }
+
+    return (
+        <div className="">
+            {skills.map((skill, i) => (
+                <div className="group border-2 border-b-0 last:border-b-2 last:rounded-b-lg first:rounded-t-lg py-4 px-6">
+                    <div className=" flex justify-between items-center ">
+                        <p className="font-bold">{skill.name}</p>
+
+                        <button
+                            className="px-4 py-1 border-2 rounded-full text-xs invisible group-hover:visible hover:border-amber-400 hover:text-amber-500 hover:bg-amber-50"
+                            onClick={() => handleClick(i)}>
+                            {tabShow === i ? 'less' : 'more'}
+                        </button>
+                    </div>
+
+
+                    <div className={`${tabShow === i ? 'max-h-96 duration-700' : 'max-h-0 duration-300'} overflow-hidden text-justify transition-all ease-in-out `}>
+                        <p className="text-xs pt-2">{skill.desc}</p>
+                    </div>
+
+
+                </div>
+            ))}
+
+        </div>
+    )
+}
+
+export default SkillAccordion
--- src/lib/skills.js
+++ src/lib/skills.js
@@ -2,26 +2,31 @@ export const skills = [
     {
         name: "Next.JS 13",
         value: 90,
-        fullMark: 100
+        fullMark: 100,
+        desc: "Currently, I made some projects using Javascript (React JS) Framework - Next JS 13. Next JS is so powerful framework that I have known. it can do front-end and back-end in a single application. the most feature that I amaze is the layouting method. we can make a component that can be rendered only once and that component is not gonna rendered again even we move to another page. this method can drastically increase web page loadtime. It's in Next.JS 13 only."
     },
     {
         name: "React JS",
         value: 60,
-        fullMark: 100
+        fullMark: 100,
+        desc: "React is javascript library. I always use react in my front-end application"
     },
     {
         name: "Laravel",
         value: 60,
-        fullMark: 100
+        fullMark: 100,
+        desc: "Laravel is very powerful PHP framework. laravel is good for ERP application and many CRUD use cases"
     },
     {
-        name: "MySQL",
+        name: "Database",
         value: 80,
-        fullMark: 100
+        fullMark: 100,
+        desc: "MySQL is my first database I've learned. It's easy to manage via phpmyadmin. Database query is perfect if we combine with ORM such as Eloquent Laravel or Prisma JS"
     },
     {
         name: "Tailwind CSS",
         value: 90,
-        fullMark: 100
+        fullMark: 100,
+        desc: "Tailwind is good to use if we implement in component layout method such as React. Easy to configure and good for more complex use cases."
     }
 ]
