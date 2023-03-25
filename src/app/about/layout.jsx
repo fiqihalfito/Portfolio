1f5147955f20cb85bccdad9eea85b2ef27483a11
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-03-25 22:01:05 +0700

    add about layout

---
+++ src/app/about/layout.jsx
@@ -0,0 +1,15 @@
+import NavBar from "../_root_components/NavBar"
+
+export const metadata = {
+    title: 'About',
+    description: 'Portfolio by Fiqih Alfito',
+}
+
+export default function AboutLayout({ children }) {
+    return (
+        <div>
+            <NavBar segment={'about'} />
+            {children}
+        </div>
+    )
+}
