d0fa2c49c361ded5cc9d012f64af06362023c595
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-03-26 14:06:59 +0700

    added scrollIntoView

--- src/app/portfolio/_portfolio_components/PortfolioNav.jsx
+++ src/app/portfolio/_portfolio_components/PortfolioNav.jsx
@@ -6,26 +6,34 @@ const PortfolioNav = () => {
 
     const segment = useSelectedLayoutSegment()
 
+    const focusSelected = (e) => {
+        e.target.scrollIntoView({ block: "end", inline: "nearest", behavior: "smooth" })
+    }
+
     return (
-        <div className='flex justify-start md:justify-center gap-x-8 overflow-x-auto '>
+        <div className='flex justify-start md:justify-center gap-x-8 overflow-x-auto snap-mandatory snap-x '>
             <Link
                 href={'/portfolio'}
-                className={`${segment === null && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap  `}>
+                onFocus={focusSelected}
+                className={`${segment === null && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap  snap-start`}>
                 All
             </Link>
             <Link
                 href={'/portfolio/front-end'}
-                className={`${segment === 'front-end' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap `}>
+                onFocus={focusSelected}
+                className={`${segment === 'front-end' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-end`}>
                 Front End
             </Link>
             <Link
                 href={'/portfolio/back-end'}
-                className={`${segment === 'back-end' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap `}>
+                onFocus={focusSelected}
+                className={`${segment === 'back-end' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-center`}>
                 Back End
             </Link>
             <Link
                 href={'/portfolio/machine-learning'}
-                className={`${segment === 'machine-learning' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap `}>
+                onFocus={focusSelected}
+                className={`${segment === 'machine-learning' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-end`}>
                 Machine Learning
             </Link>
         </div>
