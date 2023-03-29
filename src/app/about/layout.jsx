90061639e342b7f1ef0d8a7d6a01bef6c24847bd
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-03-29 12:25:12 +0700

    add about

--- src/app/about/layout.jsx
+++ src/app/about/layout.jsx
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function AboutLayout({ children }) {
     return (
-        <div>
+        <div className="">
             <NavBar segment={'about'} />
             {children}
         </div>
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,8 @@
 const About = () => {
     return (
-        <div>
-            this page is under development
+        <div className="md:custom-container">
+            <p className="text-5xl font-bold mb-4">About</p>
+            <p className="indent-8 text-justify">HI! My name is Fiqih Alfito. I'm Full Stack developer. I had built several apps from some technologies such as Next.JS 13, React, Laravel, MySQL, and Tailwind CSS. Focusing on front-end development and back-end development especially in Next.JS 13. I am self-motivated person, fast learner, and open to new challenges for development of my skills.</p>
         </div>
     )
 }
