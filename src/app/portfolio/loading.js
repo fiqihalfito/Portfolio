b16ccc1dbc37f683c6d7a77a049d9e470f0e6936
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-03-29 07:21:48 +0700

    design loading for light mode

--- src/app/portfolio/loading.js
+++ src/app/portfolio/loading.js
@@ -6,13 +6,13 @@ export default function Loading() {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-12 mt-8">
             {Array.from({ length: 9 }).map((el, i) => (
                 <div key={i} className="rounded-lg overflow-hidden">
-                    <div className="h-52 bg-neutral-800">
+                    <div className="h-52 bg-slate-300 dark:bg-cyan-900">
                         {/* photo placeholder */}
                     </div>
-                    <div className="bg-neutral-900 p-8">
+                    <div className="bg-slate-400 dark:bg-slate-800 p-8">
                         {/* text placeholder */}
-                        <div className="h-4 w-full bg-neutral-500 mb-4"></div>
-                        <div className="h-4 w-1/2 bg-neutral-500"></div>
+                        <div className="h-4 w-full bg-slate-500 mb-4"></div>
+                        <div className="h-4 w-1/2 bg-slate-500"></div>
                     </div>
                 </div>
             ))}
