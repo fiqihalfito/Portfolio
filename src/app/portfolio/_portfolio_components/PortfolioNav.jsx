c9da33b08476c504f0c9e464554446340f78b48d
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-04-03 13:56:51 +0700

    add icons at portfolio menu

--- src/app/portfolio/_portfolio_components/PortfolioNav.jsx
+++ src/app/portfolio/_portfolio_components/PortfolioNav.jsx
@@ -1,6 +1,11 @@
 "use client"
+import LoveIcon from "@/assets/icons/love"
+import ColorfulMenuIcon from "@/assets/icons/colorful-menu"
 import Link from "next/link"
 import { useSelectedLayoutSegment } from "next/navigation"
+import PaletteIcon from "@/assets/icons/palette"
+import CogIcon from "@/assets/icons/cog"
+import BrainIcon from "@/assets/icons/brain"
 
 const PortfolioNav = () => {
 
@@ -11,36 +16,36 @@ const PortfolioNav = () => {
     }
 
     return (
-        <div className='flex justify-start md:justify-center gap-x-8 overflow-x-auto snap-mandatory snap-x '>
+        <div className='flex justify-start md:justify-center gap-x-8 overflow-x-auto snap-mandatoryx snap-x '>
             <Link
                 href={'/portfolio/pinned'}
                 onFocus={focusSelected}
-                className={`${segment === 'pinned' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap  snap-start`}>
-                Pinned
+                className={`${segment === 'pinned' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-start flex items-center gap-x-2`}>
+                <LoveIcon /> Pinned
             </Link>
             <Link
                 href={'/portfolio'}
                 onFocus={focusSelected}
-                className={`${segment === null && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap  snap-start`}>
-                All
+                className={`${segment === null && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap  snap-start flex items-center gap-x-2`}>
+                <ColorfulMenuIcon /> All
             </Link>
             <Link
                 href={'/portfolio/front-end'}
                 onFocus={focusSelected}
-                className={`${segment === 'front-end' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-start`}>
-                Front End
+                className={`${segment === 'front-end' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-start flex items-center gap-x-2`}>
+                <PaletteIcon /> Front End
             </Link>
             <Link
                 href={'/portfolio/back-end'}
                 onFocus={focusSelected}
-                className={`${segment === 'back-end' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-center`}>
-                Back End
+                className={`${segment === 'back-end' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-center flex items-center gap-x-2`}>
+                <CogIcon /> Back End
             </Link>
             <Link
                 href={'/portfolio/machine-learning'}
                 onFocus={focusSelected}
-                className={`${segment === 'machine-learning' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-end`}>
-                Machine Learning
+                className={`${segment === 'machine-learning' && 'border-b-2'} border-black dark:border-white p-2 hover:border-b-2 whitespace-nowrap snap-end flex items-center gap-x-2`}>
+                <BrainIcon /> Machine Learning
             </Link>
         </div>
     )
---
+++ src/assets/icons/love.jsx
@@ -0,0 +1,15 @@
+function LoveIcon() {
+    return (
+        <svg
+            viewBox="0 0 1024 1024"
+            className="h-4 md:h-6"
+        >
+            <path
+                fill="#E02D2D"
+                d="M927.4 273.5v-95.4h-87.9V82.8h-201v95.3h-87.9v95.4h-78.5v-95.4h-88V82.8H183.2v95.3H95.3v95.4H16.7v190.6h78.6v95.4h75.3v95.3H246v95.3h87.9v95.4h100.5v95.3h153.9v-95.3h100.4v-95.4h88v-95.3h75.4v-95.3h75.3v-95.4h78.5V273.5z"
+            ></path>
+        </svg>
+    );
+}
+
+export default LoveIcon;
