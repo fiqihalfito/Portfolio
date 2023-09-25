b7445851611da7a0eaa4fe7be7d55bda23cbd854
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-09-25 07:03:26 +0700

    minor

--- src/app/_root_components/ToggleDarkMode.jsx
+++ src/app/_root_components/ToggleDarkMode.jsx
@@ -1,24 +1,21 @@
-"use client"
+"use client";
 
-import MoonIcon from "@/assets/icons/moon"
-import SunIcon from "@/assets/icons/sun"
-import { useContext } from "react"
-import { ThemeContext } from "../_root_provider/theme-provider"
+import MoonIcon from "@/assets/icons/moon";
+import SunIcon from "@/assets/icons/sun";
+import { useContext } from "react";
+import { ThemeContext } from "../_root_provider/theme-provider";
 
 const ToggleDarkMode = () => {
-
-    const [theme, handleTheme] = useContext(ThemeContext)
+    const [theme, handleTheme] = useContext(ThemeContext);
 
     return (
         <button
             className={`p-2  border-sky-400 bg-sky-900 hover:border-4 dark:border-cyan-400 dark:bg-sky-600 hover:bg-sky-800 hover:border-sky-300 rounded-lg`}
-            onClick={handleTheme}>
-
-            {theme === 'light' ? <MoonIcon /> : <SunIcon />}
-
+            onClick={handleTheme}
+        >
+            {theme === "light" ? <MoonIcon /> : <SunIcon />}
         </button>
+    );
+};
 
-    )
-}
-
-export default ToggleDarkMode
+export default ToggleDarkMode;
