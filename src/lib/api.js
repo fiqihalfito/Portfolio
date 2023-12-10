7b6cd7eb3a63eaca3158ffd75fa656a9d7c8fd58
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2023-12-10 15:27:37 +0700

    nice

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,26 +1,29 @@
-
 export const getSkill = async (skill) => {
-    const response = await fetch('https://api.github.com/users/fiqihalfito/repos',
+    const response = await fetch(
+        "https://api.github.com/users/fiqihalfito/repos?per_page=100",
         {
             headers: {
-                'Authorization': `BEARER ${process.env.GITHUB_TOKEN}`
+                Authorization: `BEARER ${process.env.GITHUB_TOKEN}`,
             },
             // next: { revalidate: 0 },
-            cache: 'no-store'
-        })
+            // cache: 'no-store'
+        }
+    );
 
     // console.log(response);
 
-    let repos = await response.json()
+    let repos = await response.json();
 
     if (!skill) {
         // all repos
-        repos = repos.filter(repo => repo.topics.length > 0)
+        repos = repos.filter((repo) => repo.topics.length > 0);
     } else {
         // repos by topics/skill
-        repos = repos.filter(repo => repo.topics.includes(skill))
+        repos = repos.filter((repo) => repo.topics.includes(skill));
     }
 
-    const sortLatestRepos = repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
-    return sortLatestRepos
-}
+    const sortLatestRepos = repos.sort(
+        (a, b) => new Date(b.created_at) - new Date(a.created_at)
+    );
+    return sortLatestRepos;
+};
